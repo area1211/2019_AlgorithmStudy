c5d70f07ba53ec7af8b0dd501f31ca0dd1675b7a
Jung Inho <area1211@naver.com>
2019-03-30 14:58:05 +0900

    [DP]no1520 내리막길 성공: DFS로 풀 경우 방문표시를 함으로써 이미 탐색한 곳을 다시 탐색하지 않도록 하는 것이 중요하다.

---
+++ src/bj/no1520_내리막길.java
@@ -0,0 +1,80 @@
+package bj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// TODO: 2019-03-30
+// 방문 표시를 하지 않았을 때 이미 방문한 곳까지 다시 탐색을 하므로 시간초과가 났던 것 같다.
+// 방문 표시를 위해 Dp 배열을 -1로 초기화 하고 방문 했을 경우 0으로 변경해줌으로써 해결됐다.
+public class no1520_내리막길 {
+    static int[] dr = {0, 1, 0, -1};
+    static int[] dc = {1, 0, -1, 0};
+    static int R, C;
+    static int[][] map;
+    static int[][] dp;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        map = new int[R][C];
+        dp = new int[R][C];
+
+        for (int i = 0; i < R; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < C; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                dp[i][j] = -1;
+            }
+        }
+
+        dp(0, 0);
+
+        print();
+
+        System.out.println(dp[0][0]);
+    }
+
+    static void dp(int r, int c) {
+
+        if (r == R-1 && c == C-1){
+            dp[r][c] = 1;
+            print();
+            return;
+        }
+
+        if (dp[r][c] == -1) dp[r][c] = 0; // 방문 표시
+
+
+        for (int i = 0; i < 4; i++) {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+
+            if (nr < 0 || nc < 0 || nr > R-1 || nc > C-1) continue;
+            if (map[r][c] <= map[nr][nc]) continue;
+
+            if (dp[nr][nc] >= 0) {
+                dp[r][c] += dp[nr][nc];
+                print();
+                continue;
+            }
+
+            dp(nr, nc);
+            dp[r][c] += dp[nr][nc];
+            print();
+        }
+    }
+
+    static void print() {
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
--- src/bj/rethink/no9251_LCS.java
+++ src/bj/rethink/no9251_LCS.java
@@ -8,26 +8,56 @@ import java.io.InputStreamReader;
 // [DP]no9251 LCS 성공 : 두 개의 문자열을 동시에 비교해야하니까 2차원 배열로 풀어내는 아이디어가 신기했따.
 public class no9251_LCS {
 
+    static String str1, str2;
+    static int[][] dp;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String str1 = br.readLine();
-        String str2 = br.readLine();
+        str1 = br.readLine();
+        str2 = br.readLine();
+        dp = new int[str2.length() + 1][str1.length() + 1];
 
-        int[][] dp = new int[str2.length() + 1][str1.length() + 1];
-
-        for (int i = 1; i <= str2.length(); i++) {
-            for (int j = 1; j <= str1.length(); j++) {
-
-                if (str1.charAt(j - 1) == str2.charAt(i - 1))
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                else
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-
+        // 초기화. 0행 0열은 0으로, 아직 구해지지 않은 dp는 -1로
+        for (int i = 1; i < str2.length() + 1; i++) {
+            for (int j = 1; j < str1.length() + 1; j++) {
+                dp[i][j] = -1;
             }
         }
 
-        System.out.println(dp[str2.length()][str1.length()]);
 
+        // top-down
+        System.out.println(dp(str2.length(), str1.length()));
+
+        // bottom-up
+
+//
+//        for (int i = 1; i <= str2.length(); i++) {
+//            for (int j = 1; j <= str1.length(); j++) {
+//
+//                if (str1.charAt(j - 1) == str2.charAt(i - 1))
+//                    dp[i][j] = dp[i - 1][j - 1] + 1;
+//                else
+//                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+//
+//            }
+//        }
+//
+//        System.out.println(dp[str2.length()][str1.length()]);
+
+    }
+
+    static int dp(int i, int j) {
+        if (i < 0 || j < 0)
+            return 0;
+        else if (dp[i][j] != -1)
+            return dp[i][j];
+
+        if (str1.charAt(j - 1) == str2.charAt(i - 1))
+            dp[i][j] = dp(i - 1, j - 1) + 1;
+        else
+            dp[i][j] = Math.max(dp(i-1,j), dp(i, j-1));
+
+        return dp[i][j];
     }
 }
