771ab2f9a5fff3b7c79514e67a16aee435884350
Jung Inho <area1211@naver.com>
2019-04-23 15:45:51 +0900

    [DP]no2631 줄세우기 수정 : Arrays.binarySearch와 배열을 사용하니까 속도가 더 빨라졌다.

--- src/bj/no2631_줄세우기.java
+++ src/bj/no2631_줄세우기.java
@@ -3,17 +3,18 @@ package bj;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 // TODO: 2019-04-23
 public class no2631_줄세우기 {
 
     static int[] kids = new int[201];
+    static int[] dp = new int[201];
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println(10>>1);
         int N = Integer.parseInt(br.readLine());
 
 
@@ -22,20 +23,45 @@ public class no2631_줄세우기 {
         }
 
 
-        List<Integer> list = new ArrayList<>();
+//        List<Integer> list = new ArrayList<>();
+//        list.add(kids[1]);
 
-        for (int i = 1; i <= N; i++) {
+        dp[0] = kids[1];
+        int count = 1;
 
-            int searchResult = binarySearch(list, kids[i]);
-            if (searchResult + 1 < list.size() && list.get(searchResult + 1) > kids[i]) {
-                list.set(searchResult + 1, kids[i]);
-            } else {
-                list.add(kids[i]);
+        for (int i = 2; i <= N; i++) {
+
+//            int searchResult = binarySearch(list, kids[i]);
+//            if (searchResult + 1 < list.size() && list.get(searchResult + 1) > kids[i]) {
+//                list.set(searchResult + 1, kids[i]);
+//            } else {
+//                list.add(kids[i]);
+//            }
+
+            //ArrayList를 이용한 이진탐색
+//            int searchResult = Arrays.binarySearch(list.toArray(), kids[i]);
+//            searchResult = -searchResult - 1;
+//
+//            if (searchResult < list.size() && list.get(searchResult) > kids[i]) {
+//                list.set(searchResult, kids[i]);
+//            }
+//            else {
+//                list.add(kids[i]);
+//            }
+
+            // 배열을 이용한 이진탐색
+            int searchResult = Arrays.binarySearch(dp, 0, count - 1, kids[i]);
+            searchResult = -searchResult - 1;
+            if (searchResult < count && dp[searchResult] > kids[i]) {
+                dp[searchResult] =  kids[i];
             }
-
+            else {
+                dp[count++] = kids[i];
+            }
         }
 
-        int LISCount = list.size();
+//        int LISCount = list.size();
+        int LISCount = count;
 
         System.out.println(N - LISCount);
     }
